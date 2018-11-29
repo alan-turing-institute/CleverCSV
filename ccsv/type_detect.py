4d9a756e60897e201073e4d8107f50ddf93526fd
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2018-11-29 14:57:11 +0000

    Add type detection

---
+++ ccsv/type_detect.py
@@ -0,0 +1,308 @@
+# -*- coding: utf-8 -*-
+
+"""
+Code for computing the type score.
+
+Author: Gertjan van den Burg
+
+"""
+
+import regex
+
+DEFAULT_EPS_TYPE = 1e-10
+
+# Used this site: https://unicode-search.net/unicode-namesearch.pl
+SPECIALS_ALLOWED = [
+    # Periods
+    "\u002e",
+    "\u06d4",
+    "\u3002",
+    "\ufe52",
+    "\uff0e",
+    "\uff61",
+    # Parentheses
+    "\u0028",
+    "\u0029",
+    "\u27ee",
+    "\u27ef",
+    "\uff08",
+    "\uff09",
+    # Question marks
+    "\u003F",
+    "\u00BF",
+    "\u037E",
+    "\u055E",
+    "\u061F",
+    "\u1367",
+    "\u1945",
+    "\u2047",
+    "\u2048",
+    "\u2049",
+    "\u2CFA",
+    "\u2CFB",
+    "\u2E2E",
+    "\uA60F",
+    "\uA6F7",
+    "\uFE16",
+    "\uFE56",
+    "\uFF1F",
+    chr(69955),  # chakma question mark
+    chr(125279),  # adlam initial question mark
+    # Exclamation marks
+    "\u0021",
+    "\u00A1",
+    "\u01C3",
+    "\u055C",
+    "\u07F9",
+    "\u109F",
+    "\u1944",
+    "\u203C",
+    "\u2048",
+    "\u2049",
+    "\uAA77",
+    "\uFE15",
+    "\uFE57",
+    "\uFF01",
+    chr(125278),  # adlam initial exclamation mark
+]
+
+PATTERNS = {
+    "number_1": "(?=[+-\.\d])[+-]?(?:0|[1-9]\d*)?(((?P<dot>\.)?(?(dot)(?P<yes_dot>\d*(\d+[eE][+-]?\d+)?)|(?P<no_dot>([eE][+-]?\d+)?)))|((?P<comma>,)?(?(comma)(?P<yes_comma>\d+(\d+[eE][+-]?\d+)?)|(?P<no_comma>([eE][+-]?\d+)?))))",
+    "number_2": "[+-]?(?:[1-9]|[1-9]\d{0,2})(?:\,\d{3})+\.\d*",
+    "number_3": "[+-]?(?:[1-9]|[1-9]\d{0,2})(?:\.\d{3})+\,\d*",
+    "url": "(?:(?:[A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)(?:(?:\/[\+~%\/.\w\-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?",
+    "email": r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)",
+    "unicode_alphanum": "(\p{N}+\p{L}+[\p{N}\p{L}\ "
+    + regex.escape("".join(SPECIALS_ALLOWED))
+    + "]*|\p{L}+[\p{N}\p{L}\ "
+    + regex.escape("".join(SPECIALS_ALLOWED))
+    + "]+)",
+    "time_hhmmss": "(0[0-9]|1[0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])",
+    "time_hhmm": "(0[0-9]|1[0-9]|2[0-3]):([0-5][0-9])",
+    "time_HHMM": "(0[0-9]|1[0-9]|2[0-3])([0-5][0-9])",
+    "time_HH": "(0[0-9]|1[0-9]|2[0-3])([0-5][0-9])",
+    "time_hmm": "([0-9]|1[0-9]|2[0-3]):([0-5][0-9])",
+    "currency": "\p{Sc}\s?(.*)",
+    "unix_path": "[\/~]{1,2}(?:[a-zA-Z0-9\.]+(?:[\/]{1,2}))+(?:[a-zA-Z0-9\.]+)",
+}
+
+
+class TypeDetector(object):
+    def __init__(self, strip_whitespace=True):
+        self.patterns = PATTERNS
+        self.strip_whitespace = strip_whitespace
+
+        self._init_date_patterns()
+        self._compile_regexes()
+
+    def _init_date_patterns(self):
+        """
+        Generate the date regular expressions.
+        """
+        year2 = "(?:\d{2})"
+        year4 = "(?:[12]\d{3})"
+
+        month_leading = "(?:0[1-9]|1[0-2])"
+        month_sparse = "(?:[1-9]|1[0-2])"
+
+        day_leading = "(?:0[1-9]|[12]\d|3[01])"
+        day_sparse = "(?:[1-9]|[12]\d|3[01])"
+
+        sep = "[-/\.\ ]"
+
+        counter = 0
+        for year in [year2, year4]:
+            for month in [month_leading, month_sparse]:
+                for day in [day_leading, day_sparse]:
+                    fmt = {"year": year, "month": month, "day": day, "sep": sep}
+
+                    pat_1 = "{year}{sep}{month}{sep}{day}".format(**fmt)
+                    pat_2 = "{day}{sep}{month}{sep}{year}".format(**fmt)
+                    pat_3 = "{month}{sep}{day}{sep}{year}".format(**fmt)
+
+                    pat_cn = "{year}年{month}月{day}日".format(**fmt)
+                    pat_ko = "{year}년{month}월{day}일".format(**fmt)
+
+                    for pattern in [pat_1, pat_2, pat_3, pat_cn, pat_ko]:
+                        self.patterns["date_%i" % counter] = pattern
+                        counter += 1
+
+        # These should be allowed as dates, but are also numbers.
+        for year in [year2, year4]:
+            fmt = {
+                "year": year,
+                "month": month_leading,
+                "day": day_leading,
+                "sep": "",
+            }
+            pat_1 = "{year}{sep}{month}{sep}{day}".format(**fmt)
+            pat_2 = "{day}{sep}{month}{sep}{year}".format(**fmt)
+            pat_3 = "{month}{sep}{day}{sep}{year}".format(**fmt)
+
+            for pattern in [pat_1, pat_2, pat_3, pat_cn]:
+                self.patterns["date_%i" % counter] = pattern
+                counter += 1
+
+    def _compile_regexes(self):
+        for key, value in self.patterns.items():
+            self.patterns[key] = regex.compile(value)
+
+    def is_known_type(self, cell):
+        return not self.detect_type(cell) is None
+
+    def detect_type(self, cell):
+        type_tests = [
+            ("empty", self.is_empty),
+            ("url_or_email", self.is_url_or_email),
+            ("number", self.is_number),
+            ("time", self.is_time),
+            ("percentage", self.is_percentage),
+            ("currency", self.is_currency),
+            ("unicode_alphanum", self.is_unicode_alphanum),
+            ("nan", self.is_nan),
+            ("date", self.is_date),
+            ("datetime", self.is_datetime),
+        ]
+        for name, func in type_tests:
+            if func(cell):
+                return name
+        return None
+
+    def _run_regex(self, cell, patname):
+        if self.strip_whitespace:
+            cell = cell.strip(" ")
+        pat = self.patterns.get(patname, None)
+        match = pat.fullmatch(cell)
+        return match is not None
+
+    def is_number(self, cell):
+        if cell == "":
+            return False
+        if self._run_regex(cell, "number_1"):
+            return True
+        if self._run_regex(cell, "number_2"):
+            return True
+        if self._run_regex(cell, "number_3"):
+            return True
+        return False
+
+    def is_url_or_email(self, cell):
+        return self._run_regex(cell, "url") or self._run_regex(cell, "email")
+
+    def is_unicode_alphanum(self, cell):
+        return self._run_regex(cell, "unicode_alphanum")
+
+    def is_date(self, cell):
+        if self.is_numberr(cell):
+            return False
+
+        for patname in self.patterns:
+            if patname.startswith("date_"):
+                if self._run_regex(cell, patname):
+                    return True
+        return False
+
+    def is_time(self, cell):
+        return (
+            self._run_regex(cell, "time_hmm")
+            or self._run_regex(cell, "time_hhmm")
+            or self._run_regex(cell, "time_hhmmss")
+        )
+
+    def is_empty(self, cell):
+        if self.strip_whitespace:
+            cell = cell.strip(" ")
+        return cell == ""
+
+    def is_percentage(self, cell):
+        if self.strip_whitespace:
+            cell = cell.strip(" ")
+        return cell.endswith("%") and self.is_number(cell.rstrip("%"))
+
+    def is_currency(self, cell):
+        if self.strip_whitespace:
+            cell = cell.strip(" ")
+        pat = self.patterns.get("currency", None)
+        m = pat.fullmatch(cell)
+        if m is None:
+            return False
+        grp = m.group(1)
+        if not self.is_number(grp):
+            return False
+        return True
+
+    def is_datetime(self, cell):
+        # Takes care of cells with '[date] [time]' and '[date]T[time]' (iso)
+        if " " in cell:
+            parts = cell.split(" ")
+            if len(parts) > 2:
+                return False
+            return self.is_date(parts[0]) and self.is_time(parts[1])
+        elif "T" in cell:
+            parts = cell.split("T")
+            if len(parts) > 2:
+                return False
+            isdate = self.is_date(parts[0])
+            if not isdate:
+                return False
+            # [date]T[time]
+            if self.is_time(parts[1]):
+                return True
+            # [date]T[time][+-][time]
+            if "+" in parts[1]:
+                subparts = parts[1].split("+")
+                istime1 = self.is_time(subparts[0])
+                istime2 = self.is_time(subparts[1])
+                if not istime1:
+                    return False
+                if istime2:
+                    return True
+                if self._run_regex(subparts[1], "time_HHMM"):
+                    return True
+                if self._run_regex(subparts[1], "time_HH"):
+                    return True
+            elif "-" in parts[1]:
+                subparts = parts[1].split("-")
+                istime1 = self.is_time(subparts[0])
+                istime2 = self.is_time(subparts[1])
+                if not istime1:
+                    return False
+                if istime2:
+                    return True
+                if self._run_regex(subparts[1], "time_HHMM"):
+                    return True
+                if self._run_regex(subparts[1], "time_HH"):
+                    return True
+        return False
+
+    def is_nan(self, cell):
+        if self.strip_whitespace:
+            cell = cell.strip(" ")
+        # other forms (na and nan) are caught by unicode_alphanum
+        if cell.lower() == "n/a":
+            return True
+        return False
+
+
+def gen_known_type(cells):
+    """
+    Utility that yields a generator over whether or not the provided cells are 
+    of a known type or not.
+    """
+    td = TypeDetector()
+    for cell in cells:
+        yield td.is_known_type(cell)
+
+
+def type_score(cells, eps=DEFAULT_EPS_TYPE):
+    """
+    Compute the type score as the ratio of cells with a known type.
+    """
+    total = 0
+    known = 0
+    for is_known in gen_known_type(cells):
+        total += 1
+        known += is_known
+    if total == 0:
+        return eps
+    return max(eps, known / total)
---
+++ tests/test_type_detect.py
@@ -0,0 +1,235 @@
+# -*- coding: utf-8 -*-
+
+"""
+Unit tests for the type detection.
+
+Author: Gertjan van den Burg
+
+"""
+
+import unittest
+
+from ccsv.type_detect import TypeDetector
+
+
+class TypeDetectorTestCase(unittest.TestCase):
+    def setUp(self):
+        self.td = TypeDetector()
+
+    """
+    NUMBERS
+    """
+
+    def test_number_true_1(self):
+        self.assertTrue(self.td.is_number("1"))
+
+    def test_number_true_2(self):
+        self.assertTrue(self.td.is_number("2"))
+
+    def test_number_true_3(self):
+        self.assertTrue(self.td.is_number("34"))
+
+    def test_number_true_4(self):
+        self.assertTrue(self.td.is_number("56"))
+
+    def test_number_true_5(self):
+        self.assertTrue(self.td.is_number("123"))
+
+    def test_number_true_6(self):
+        self.assertTrue(self.td.is_number("789"))
+
+    def test_number_true_7(self):
+        self.assertTrue(self.td.is_number("132."))
+
+    def test_number_true_8(self):
+        self.assertTrue(self.td.is_number("0.123"))
+
+    def test_number_true_9(self):
+        self.assertTrue(self.td.is_number("0.10800212"))
+
+    def test_number_true_10(self):
+        self.assertTrue(self.td.is_number("0.1231e-087"))
+
+    def test_number_true_11(self):
+        self.assertTrue(self.td.is_number("10.789e09"))
+
+    def test_number_true_12(self):
+        self.assertTrue(self.td.is_number("123.256e+08"))
+
+    def test_number_true_13(self):
+        self.assertTrue(self.td.is_number("0.1231E-087"))
+
+    def test_number_true_14(self):
+        self.assertTrue(self.td.is_number("10.789E09"))
+
+    def test_number_true_15(self):
+        self.assertTrue(self.td.is_number("123.256E+08"))
+
+    def test_number_true_16(self):
+        self.assertTrue(self.td.is_number("123,456,798.00"))
+
+    def test_number_true_17(self):
+        self.assertTrue(self.td.is_number("23,456.798"))
+
+    def test_number_true_18(self):
+        self.assertTrue(self.td.is_number("1,234.56"))
+
+    def test_number_true_19(self):
+        self.assertTrue(self.td.is_number("1,123."))
+
+    def test_number_true_20(self):
+        self.assertTrue(self.td.is_number("1e5"))
+
+    def test_number_true_21(self):
+        self.assertTrue(self.td.is_number("1.23e5"))
+
+    def test_number_true_22(self):
+        self.assertTrue(self.td.is_number("-1"))
+
+    def test_number_true_23(self):
+        self.assertTrue(self.td.is_number("-2"))
+
+    def test_number_true_24(self):
+        self.assertTrue(self.td.is_number("-34"))
+
+    def test_number_true_25(self):
+        self.assertTrue(self.td.is_number("-56"))
+
+    def test_number_true_26(self):
+        self.assertTrue(self.td.is_number("-123"))
+
+    def test_number_true_27(self):
+        self.assertTrue(self.td.is_number("-789"))
+
+    def test_number_true_28(self):
+        self.assertTrue(self.td.is_number("-0.123"))
+
+    def test_number_true_29(self):
+        self.assertTrue(self.td.is_number("-0.10800212"))
+
+    def test_number_true_30(self):
+        self.assertTrue(self.td.is_number("-0.1231e-087"))
+
+    def test_number_true_31(self):
+        self.assertTrue(self.td.is_number("-10.789e09"))
+
+    def test_number_true_32(self):
+        self.assertTrue(self.td.is_number("-123.256e+08"))
+
+    def test_number_true_33(self):
+        self.assertTrue(self.td.is_number("-0.1231E-087"))
+
+    def test_number_true_34(self):
+        self.assertTrue(self.td.is_number("-10.789E09"))
+
+    def test_number_true_35(self):
+        self.assertTrue(self.td.is_number("-123.256E+08"))
+
+    def test_number_true_36(self):
+        self.assertTrue(self.td.is_number("-123,456,798.00"))
+
+    def test_number_true_37(self):
+        self.assertTrue(self.td.is_number("-23,456.798"))
+
+    def test_number_true_38(self):
+        self.assertTrue(self.td.is_number("-1,234.56"))
+
+    def test_number_true_39(self):
+        self.assertTrue(self.td.is_number("+1"))
+
+    def test_number_true_40(self):
+        self.assertTrue(self.td.is_number("+2"))
+
+    def test_number_true_41(self):
+        self.assertTrue(self.td.is_number("+34"))
+
+    def test_number_true_42(self):
+        self.assertTrue(self.td.is_number("+56"))
+
+    def test_number_true_43(self):
+        self.assertTrue(self.td.is_number("+123"))
+
+    def test_number_true_44(self):
+        self.assertTrue(self.td.is_number("+789"))
+
+    def test_number_true_45(self):
+        self.assertTrue(self.td.is_number("+0.123"))
+
+    def test_number_true_46(self):
+        self.assertTrue(self.td.is_number("+0.10800212"))
+
+    def test_number_true_47(self):
+        self.assertTrue(self.td.is_number("+0.1231e-087"))
+
+    def test_number_true_48(self):
+        self.assertTrue(self.td.is_number("+10.789e09"))
+
+    def test_number_true_49(self):
+        self.assertTrue(self.td.is_number("+123.256e+08"))
+
+    def test_number_true_50(self):
+        self.assertTrue(self.td.is_number("+0.1231E-087"))
+
+    def test_number_true_51(self):
+        self.assertTrue(self.td.is_number("+10.789E09"))
+
+    def test_number_true_52(self):
+        self.assertTrue(self.td.is_number("+123.256E+08"))
+
+    def test_number_true_53(self):
+        self.assertTrue(self.td.is_number("+123,456,798.00"))
+
+    def test_number_true_54(self):
+        self.assertTrue(self.td.is_number("+23,456.798"))
+
+    def test_number_true_55(self):
+        self.assertTrue(self.td.is_number("+1,234.56"))
+
+    def test_number_true_56(self):
+        self.assertTrue(self.td.is_number(".707"))
+
+    def test_number_true_57(self):
+        self.assertTrue(self.td.is_number("-.707"))
+
+    def test_number_true_58(self):
+        self.assertTrue(self.td.is_number("50,000.123"))
+
+    def test_number_true_59(self):
+        self.assertTrue(self.td.is_number("1.000,123"))
+
+    def test_number_false_1(self):
+        self.assertFalse(self.td.is_number("0000.213654"))
+
+    def test_number_false_2(self):
+        self.assertFalse(self.td.is_number("123.465.798"))
+
+    def test_number_false_3(self):
+        self.assertFalse(self.td.is_number("0.5e0.5"))
+
+    def test_number_false_4(self):
+        self.assertFalse(self.td.is_number("1,23.45"))
+
+    def test_number_false_5(self):
+        self.assertFalse(self.td.is_number("12,34.56"))
+
+    def test_number_false_6(self):
+        self.assertFalse(self.td.is_number("+00003"))
+
+    def test_number_false_7(self):
+        self.assertFalse(self.td.is_number("37.e88"))
+
+    def test_number_false_8(self):
+        self.assertFalse(self.td.is_number("0,132.6"))
+
+    def test_number_false_9(self):
+        self.assertFalse(self.td.is_number("1,"))
+
+    def test_number_false_10(self):
+        self.assertFalse(self.td.is_number(""))
+
+    def test_number_false_11(self):
+        self.assertFalse(self.td.is_number("E14000537"))
+
+
+if __name__ == "__main__":
+    unittest.main()
