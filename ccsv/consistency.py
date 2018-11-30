5cd4813945cf57163e142781c4951e988fde85df
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2018-11-30 16:54:58 +0000

    Add code for dialect detection with consistency measure

---
+++ ccsv/consistency.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+"""
+Detect the dialect using the data consistency measure.
+
+Author: Gertjan van den Burg
+
+"""
+
+from .potential_dialects import get_dialects
+from .detect_pattern import pattern_score
+from .detect_type import type_score
+from .break_ties import tie_breaker
+
+
+def detect_dialect_consistency(data):
+    dialects = get_dialects(data)
+    Qmax = -float("inf")
+
+    H = set()
+
+    for dialect in sorted(dialects):
+        P = pattern_score(data, dialect)
+        if P < Qmax:
+            continue
+        T = type_score(data, dialect)
+        Q = P * T
+        if Q > Qmax:
+            H = set([dialect])
+        elif Q == Qmax:
+            H.add(dialect)
+
+    if len(H) == 1:
+        return H.pop()
+    return tie_breaker(data, list(H))
