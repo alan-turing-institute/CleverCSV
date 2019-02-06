8ae104c29470e04351f812ae02aaa4c1f2e1ae73
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2019-02-06 17:14:39 +0000

    Organise init

--- python/ccsv/__init__.py
+++ python/ccsv/__init__.py
@@ -1,13 +1,12 @@
 name = "clever_csv"
 
-from .read import reader
-from .write import writer
+from csv import QUOTE_ALL, QUOTE_MINIMAL, QUOTE_NONNUMERIC, QUOTE_NONE
 
 from .detect import Detector
 from .detect import Detector as Sniffer
-
-from .parser import field_size_limit
-
+from .dict_read_write import DictReader, DictWriter
 from .exceptions import Error
-
-from csv import QUOTE_ALL, QUOTE_MINIMAL, QUOTE_NONNUMERIC, QUOTE_NONE
+from .parser import field_size_limit
+from .read import reader
+from .wrappers import read_as_dataframe, read_as_dicts
+from .write import writer
