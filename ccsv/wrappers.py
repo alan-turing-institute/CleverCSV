4518b74cb04b67a246ece8464e790ac9bb506e19
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2019-05-02 16:13:11 +0100

    Remove deprecated function

--- ccsv/wrappers.py
+++ ccsv/wrappers.py
@@ -20,16 +20,6 @@ from .utils import get_encoding
 from .read import reader
 
 
-def read_as_dataframe(filename, dialect=None, verbose=False):
-    enc = get_encoding(filename)
-    with open(filename, "r", newline="", encoding=enc) as fid:
-        if dialect is None:
-            dialect = Detector().detect(fid.read(), verbose=verbose)
-            fid.seek(0)
-        r = reader(fid, dialect)
-        rows = list(r)
-    return pd.DataFrame.from_records(rows)
-
 
 def read_as_dicts(filename, dialect=None, verbose=False):
     enc = get_encoding(filename)
