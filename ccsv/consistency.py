45299768ab2d7dd95b22a2e30007f067d4e9f764
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2019-05-03 11:39:37 +0100

    Simplify logging function in detection

--- ccsv/consistency.py
+++ ccsv/consistency.py
@@ -40,23 +40,26 @@ def detect_dialect_consistency(data, delimiters=None, verbose=False):
 
     """
 
+    # wrapper for the print function
+    log = lambda *a, **kw: print(*a, **kw) if verbose else None
+
+    # Get potential dialects
     dialects = get_dialects(data, delimiters=delimiters)
-    if verbose:
-        print("Considering %i dialects." % len(dialects))
+    log("Considering %i dialects." % len(dialects))
     Qmax = -float("inf")
 
     H = set()
 
+    # increase the field size limit to the max expected
     old_limit = field_size_limit(len(data) + 1)
 
     for dialect in sorted(dialects):
         P = pattern_score(data, dialect)
         if P < Qmax:
-            if verbose:
-                print(
-                    "%15r:\tP = %15.6f\tT = %15.6f\tQ = %15.6f"
-                    % (dialect, P, float("nan"), float("nan"))
-                )
+            log(
+                "%15r:\tP = %15.6f\tT = %15.6f\tQ = %15.6f"
+                % (dialect, P, float("nan"), float("nan"))
+            )
             continue
         T = type_score(data, dialect)
         Q = P * T
@@ -66,10 +69,7 @@ def detect_dialect_consistency(data, delimiters=None, verbose=False):
         elif Q == Qmax:
             H.add(dialect)
 
-        if verbose:
-            print(
-                "%15r:\tP = %15.6f\tT = %15.6f\tQ = %15.6f" % (dialect, P, T, Q)
-            )
+        log("%15r:\tP = %15.6f\tT = %15.6f\tQ = %15.6f" % (dialect, P, T, Q))
 
     if len(H) == 1:
         result = H.pop()
