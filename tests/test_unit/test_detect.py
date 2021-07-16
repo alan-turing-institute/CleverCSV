a9e37f022b56e99a62be126506b3b35351d78bf0
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2021-07-16 13:06:08 +0100

    add unit tests for json and bytearray types

--- tests/test_unit/test_detect.py
+++ tests/test_unit/test_detect.py
@@ -63,6 +63,27 @@ Stonecutters Seafood and Chop House+ Lemont+ IL+ 12/19/02+ Week Back
 'Stonecutters ''Seafood'' and Chop House'+ 'Lemont'+ 'IL'+ '12/19/02'+ 'Week Back'
 """
 
+    sample10 = """\
+bytearray(b'fake data'),20:53:06,2019-09-01T19:28:21
+bytearray(b'fake data'),19:33:15,2005-02-15T19:10:31
+bytearray(b'fake data'),10:43:05,1992-10-12T14:49:24
+bytearray(b'fake data'),10:36:49,1999-07-18T17:27:55
+bytearray(b'fake data'),03:33:35,1982-04-24T17:38:45
+bytearray(b'fake data'),14:49:47,1983-01-05T22:17:42
+bytearray(b'fake data'),10:35:30,2006-10-27T02:30:45
+"""
+
+    sample11 = """\
+"{""fake"": ""json"", ""fake2"":""json2""}",13:31:38,06:00:04+01:00
+"{""fake"": ""json"", ""fake2"":""json2""}",22:13:29,14:20:11+02:00
+"{""fake"": ""json"", ""fake2"":""json2""}",04:37:27,22:04:28+03:00
+"{""fake"": ""json"", ""fake2"":""json2""}",04:25:28,23:12:53+01:00
+"{""fake"": ""json"", ""fake2"":""json2""}",21:04:15,08:23:58+02:00
+"{""fake"": ""json"", ""fake2"":""json2""}",10:37:03,11:06:42+05:30
+"{""fake"": ""json"", ""fake2"":""json2""}",10:17:24,23:38:47+06:00
+"{""fake"": ""json"", ""fake2"":""json2""}",00:02:51,20:04:45-06:00
+"""
+
     def test_detect(self):
         # Adapted from CPython
         detector = Detector()
@@ -99,6 +120,12 @@ Stonecutters Seafood and Chop House+ Lemont+ IL+ 12/19/02+ Week Back
         dialect = detector.detect(self.sample9)
         self.assertEqual(dialect.delimiter, "+")
         self.assertEqual(dialect.quotechar, "'")
+        dialect = detector.detect(self.sample10)
+        self.assertEqual(dialect.delimiter, ",")
+        self.assertEqual(dialect.quotechar, '')
+        dialect = detector.detect(self.sample11)
+        self.assertEqual(dialect.delimiter, ",")
+        self.assertEqual(dialect.quotechar, '"')
 
     def test_has_header(self):
         detector = Detector()
