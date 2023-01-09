35051d1d6115156bf6eabd6dfcbffc4c6cee4312
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2023-01-09 23:32:51 +0000

    Skip cchardet tests on windows (no wheels yet)

--- tests/test_unit/test_encoding.py
+++ tests/test_unit/test_encoding.py
@@ -10,6 +10,7 @@ This file is part of CleverCSV.
 """
 
 import os
+import platform
 import tempfile
 import unittest
 
@@ -18,6 +19,25 @@ from clevercsv.write import writer
 
 
 class EncodingTestCase(unittest.TestCase):
+
+    cases = [
+        {
+            "table": [["Å", "B", "C"], [1, 2, 3], [4, 5, 6]],
+            "encoding": "ISO-8859-1",
+            "cchardet_encoding": "WINDOWS-1252",
+        },
+        {
+            "table": [["A", "B", "C"], [1, 2, 3], [4, 5, 6]],
+            "encoding": "ascii",
+            "cchardet_encoding": "ASCII",
+        },
+        {
+            "table": [["亜唖", "娃阿", "哀愛"], [1, 2, 3], ["挨", "姶", "葵"]],
+            "encoding": "ISO-2022-JP",
+            "cchardet_encoding": "ISO-2022-JP",
+        },
+    ]
+
     def setUp(self):
         self._tmpfiles = []
 
@@ -37,44 +57,30 @@ class EncodingTestCase(unittest.TestCase):
         self._tmpfiles.append(tmpfname)
         return tmpfname
 
-    def test_encoding_1(self):
-        table = [["Å", "B", "C"], [1, 2, 3], [4, 5, 6]]
-        encoding = "ISO-8859-1"
-        tmpfname = self._build_file(table, encoding)
+    def test_encoding_chardet(self):
+        for case in self.cases:
+            table = case["table"]
+            encoding = case["encoding"]
+            with self.subTest(encoding=encoding):
+                tmpfname = self._build_file(table, encoding)
+                detected = get_encoding(tmpfname, try_cchardet=False)
+                self.assertEqual(encoding, detected)
 
-        # Test using chardet
-        detected = get_encoding(tmpfname, try_cchardet=False)
-        self.assertEqual(encoding, detected)
-
-        # Test using cChardet
-        detected = get_encoding(tmpfname, try_cchardet=True)
-        self.assertEqual("WINDOWS-1252", detected)
-
-    def test_encoding_2(self):
-        table = [["A", "B", "C"], [1, 2, 3], [4, 5, 6]]
-        encoding = "ascii"
-        tmpfname = self._build_file(table, encoding)
-
-        # Test using chardet
-        detected = get_encoding(tmpfname, try_cchardet=False)
-        self.assertEqual(encoding, detected)
-
-        # Test using cChardet
-        detected = get_encoding(tmpfname, try_cchardet=True)
-        self.assertEqual("ASCII", detected)
-
-    def test_encoding_3(self):
-        table = [["亜唖", "娃阿", "哀愛"], [1, 2, 3], ["挨", "姶", "葵"]]
-        encoding = "ISO-2022-JP"
-        tmpfname = self._build_file(table, encoding)
-
-        # Test using chardet
-        detected = get_encoding(tmpfname)
-        self.assertEqual(encoding, detected)
-
-        # Test using cChardet
-        detected = get_encoding(tmpfname, try_cchardet=True)
-        self.assertEqual(encoding, detected)
+    # Temporarily, until https://github.com/faust-streaming/cChardet/pull/15 is
+    # resolved.
+    @unittest.skipIf(
+        platform.system() == "Windows",
+        reason="No faust-cchardet wheels for Windows (yet)",
+    )
+    def test_encoding_cchardet(self):
+        for case in self.cases:
+            table = case["table"]
+            encoding = case["encoding"]
+            with self.subTest(encoding=encoding):
+                out_encoding = case["cchardet_encoding"]
+                tmpfname = self._build_file(table, encoding)
+                detected = get_encoding(tmpfname, try_cchardet=True)
+                self.assertEqual(out_encoding, detected)
 
 
 if __name__ == "__main__":
