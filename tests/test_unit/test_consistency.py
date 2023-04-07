82f84402d35556e09bad8e433550d5b5c53621b4
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2023-04-07 22:54:37 +0100

    Redesign consistency score calculation
    This redesign allows for caching the type detection
    results across dialects

--- tests/test_unit/test_consistency.py
+++ tests/test_unit/test_consistency.py
@@ -9,25 +9,19 @@ Author: G.J.J. van den Burg
 
 import unittest
 
-from clevercsv.consistency import get_best_set
+from clevercsv.consistency import ConsistencyDetector
+from clevercsv.consistency import ConsistencyScore
 from clevercsv.dialect import SimpleDialect
 
 
 class ConsistencyTestCase(unittest.TestCase):
     def test_get_best_set_1(self):
         scores = {
-            SimpleDialect(",", None, None): {"Q": 1.0},
-            SimpleDialect(";", None, None): {"Q": None},
-            SimpleDialect("|", None, None): {"Q": 2.0},
+            SimpleDialect(",", None, None): ConsistencyScore(P=1, T=1, Q=1),
+            SimpleDialect(";", None, None): ConsistencyScore(
+                P=1, T=None, Q=None
+            ),
+            SimpleDialect("|", None, None): ConsistencyScore(P=2, T=1, Q=2),
         }
-        H = get_best_set(scores)
-        self.assertEqual(H, set([SimpleDialect("|", None, None)]))
-
-    def test_get_best_set_2(self):
-        scores = {
-            SimpleDialect(";", None, None): {"Q": None},
-            SimpleDialect(",", None, None): {"Q": 1.0},
-            SimpleDialect("|", None, None): {"Q": 2.0},
-        }
-        H = get_best_set(scores)
+        H = ConsistencyDetector.get_best_dialects(scores)
         self.assertEqual(H, set([SimpleDialect("|", None, None)]))
